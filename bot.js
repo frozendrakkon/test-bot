2af60ef65b0838d00e272bcc606d28324adcd76e
unknown <frozendrakon@gmail.com>
2021-04-13 21:50:46 +0300

    fix

--- bot.js
+++ bot.js
@@ -53,6 +53,7 @@ function sayHello() {
     reactClickMood(arrElementsAngryEmoji, arrAngryAnswers, "angry");
   }
 }
+sayHello();
 
 function sayDontUnderstand() {
   submitBtn.onclick = function () {
